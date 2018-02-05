1c6cfd763ed99d8ac737006c098b2efca5814c71
Amelia Lewis <amelialewis01@gmail.com>
2018-02-05 12:37:03 -0500

    checkboxes, filter height, scrolling

--- public/js/main.js
+++ public/js/main.js
@@ -13,41 +13,66 @@ var marketBuildingContainer = $(".market-building-container");
 var marketMapContainer = $(".market-map-container");
 
 $(window).on("scroll", function(e) {
-  if ($(window).scrollTop() > ($(header).outerHeight() + $(marketPageIntro).outerHeight())) {
-    filterBar.addClass("fixed");
-    filterBarSpacer.show();
-  } else {
-    filterBar.removeClass("fixed");
-    filterBarSpacer.hide();
-  }
+	if ($(window).scrollTop() > ($(header).outerHeight() + $(marketPageIntro).outerHeight())) {
+		filterBar.addClass("fixed");
+		filterBarSpacer.show();
+	} else {
+		filterBar.removeClass("fixed");
+		filterBarSpacer.hide();
+	}
 });
 
 $(window).on("scroll", function(e) {
-  if ($(window).scrollTop() > ($(header).outerHeight() + $(marketPageIntro).outerHeight() + $(resultsBar).outerHeight() - 16) && $(window).width() > 1024) {
-    map.addClass("fixed");
-  } else {
-    map.removeClass("fixed");
-  }
+	if ($(window).scrollTop() > ($(header).outerHeight() + $(marketPageIntro).outerHeight() + $(resultsBar).outerHeight() - 16) && $(window).width() > 1024) {
+		map.addClass("fixed");
+	} else {
+		map.removeClass("fixed");
+	}
 });
 
 filterBtn.on("click", function(e) {
-  filters.addClass("show");
+	filters.addClass("show");
+		$('html, body').css({
+			overflow: 'hidden',
+			height: '100%'
+		});
 });
 
 filterBtnClose.on("click", function(e) {
-  filters.removeClass("show");
+	filters.removeClass("show");
+	$('html, body').css({
+		overflow: 'auto',
+		height: 'auto'
+	});
 });
 
 toggleMapView.on("click", function(e) {
-  if ($(window).width() < 1024) {
-    marketBuildingContainer.hide();
-    marketMapContainer.show();
-  }
+	if ($(window).width() < 1024) {
+		marketBuildingContainer.hide();
+		marketMapContainer.show();
+	}
 });
 
 toggleListView.on("click", function(e) {
-  if ($(window).width() < 1024) {
-    marketBuildingContainer.show();
-    marketMapContainer.hide();
-  }
+	if ($(window).width() < 1024) {
+		marketBuildingContainer.show();
+		marketMapContainer.hide();
+	}
 });
+
+$('input:checkbox').change(function(){
+	if($(this).is(':checked')) {
+		$(this).parent().addClass('selected'); 
+	} else {
+		$(this).parent().removeClass('selected')
+	}
+ });
+
+$('input:radio').change(function(){
+	if($(this).is(':checked')) {
+		$(this).parent().addClass('selected'); 
+		$(this).parent().siblings().removeClass('selected'); 
+	} else {
+		$(this).parent().removeClass('selected')
+	}
+ });
